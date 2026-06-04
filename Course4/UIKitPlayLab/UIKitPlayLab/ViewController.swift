06f94897eca412f58fe102efe7befacf2dd894c9
Tyson Pitcher <tyspitcher@gmail.com>
2026-06-04 17:06:51 -0600

    adding Journal and ToDoList assignments to repo

--- Course4/UIKitPlayLab/UIKitPlayLab/ViewController.swift
+++ Course4/UIKitPlayLab/UIKitPlayLab/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MainViewTitle: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func showCoolDogButton(_ sender: Any) {
+        
+    }
+    
+    @IBAction func ShowSneakyCatButton(_ sender: Any) {
+        
+        
+    }
 }
 
